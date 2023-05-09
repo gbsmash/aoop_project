adae493bd60121f114444814b3f74a1a20e8e2a4
Amina <e.graizel@icloud.com>
2023-05-09 14:00:39 +0400

    GUI completed(i hope)

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,3 +1,4 @@
+package src;
 
 public class Assignment {
     private Student student;
--- src/AssignmentFrame.java
+++ src/AssignmentFrame.java
@@ -1,3 +1,5 @@
+package src;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
